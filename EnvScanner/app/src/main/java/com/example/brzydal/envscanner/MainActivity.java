2fea06612420dc56a752ee814fea8eca09af094e
bojakowsky <krzysztof0bojakowski@gmail.com>
2016-04-06 02:19:04 +0200

    Main activity has been extended, bluetooth receiver implementede, switchers and buttons added

--- EnvScanner/app/src/main/java/com/example/brzydal/envscanner/MainActivity.java
+++ EnvScanner/app/src/main/java/com/example/brzydal/envscanner/MainActivity.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -17,35 +19,111 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 public class MainActivity extends Activity {
 
-    TextView mainText;
-    WifiManager mainWifi;
-    WifiReceiver receiverWifi;
-    List<ScanResult> wifiList;
+    private TextView deviceMainText;
 
-    ListView wifiScanResult;
-    ArrayList<String> wifiSSIDScanResult = new ArrayList<String>();
-    private ArrayAdapter<String> wifiScanResultAdapter;
+    private Switch wifiSwitch;
+    private Switch bluetoothSwitch;
+
+    private Button wifiButton;
+    private Button bluetoothButton;
+
+    //Wifi section
+    private WifiManager mainWifi;
+    private WifiReceiver receiverWifi;
+
+    private List<ScanResult> wifiScanResultList;
+    private ListView wifiScanResultListView;
+
+    private ArrayList<String> wifiSSIDList = new ArrayList<String>();
+    private ArrayAdapter<String> wifiArrayAdapter;
 
     StringBuilder sb = new StringBuilder();
 
+    //BlueTooth section
+    private BluetoothReceiver receiverBluetooth;
+    private ArrayList<BluetoothDevice> bluetoothScanResultList = new ArrayList<>();
+    private ListView bluetoothScanResultListView;
+
+    private ArrayList<String> bluetoothDeviceNameList = new ArrayList<String>();
+    private ArrayAdapter<String> bluetoothArrayAdapter;
+    BluetoothAdapter bluetoothAdapter;
     private final Handler handler = new Handler();
+
+
 
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        mainText = (TextView) findViewById(R.id.mainText);
+        deviceMainText = (TextView) findViewById(R.id.mainText);
 
+        wifiSwitch = (Switch) findViewById(R.id.wifiSwitcher);
+        bluetoothSwitch = (Switch) findViewById(R.id.bluetoothSwitcher);
+
+        wifiButton = (Button) findViewById(R.id.wifiButton);
+        bluetoothButton = (Button) findViewById(R.id.bluetoothButton);
+
+        wifiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    WifiConfiguration();
+                    wifiButton.setEnabled(true);
+                }
+                else wifiButton.setEnabled(true);
+            }
+        });
+
+        bluetoothSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    BluetoothConfiguration();
+                    bluetoothButton.setEnabled(true);
+                }
+                else bluetoothButton.setEnabled(false);
+            }
+        });
+
+
+        wifiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WifiScanSection.class);
+                intent.putExtra("SCAN_RESULT", wifiScanResultList.toArray());
+                startActivity(intent);
+            }
+        });
+
+
+        bluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BluetoothScanSection.class);
+                intent.putExtra("SCAN_RESULT", bluetoothScanResultList);
+                startActivity(intent);
+            }
+        });
+
+
+        doScanning();
+    }
+
+    public void WifiConfiguration()
+    {
         // Initiate wifi service manager
         mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
-        wifiScanResult = (ListView) findViewById(R.id.wifiScanResults);
+        wifiScanResultListView = (ListView) findViewById(R.id.wifiScanResults);
 
         // Check for wifi is disabled
         if (mainWifi.isWifiEnabled() == false)
@@ -57,45 +135,56 @@ public class MainActivity extends Activity {
             mainWifi.setWifiEnabled(true);
         }
 
-        // wifi scaned value broadcast receiver
-        receiverWifi = new WifiReceiver();
+        wifiArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, wifiSSIDList);
+        wifiScanResultListView.setAdapter(wifiArrayAdapter);
 
-        // Register broadcast receiver
-        // Broacast receiver will automatically call when number of wifi connections changed
-        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
-        wifiScanResultAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, wifiSSIDScanResult);
-        wifiScanResult.setAdapter(wifiScanResultAdapter);
-
-        wifiScanResult.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        wifiScanResultListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ScanResult scanResult = wifiList.get(position);
+                ScanResult scanResult = wifiScanResultList.get(position);
                 Intent intent = new Intent(MainActivity.this, WifiScanDetails.class);
                 intent.putExtra("SCAN_RESULT", scanResult);
                 startActivity(intent);
             }
         });
 
-        doInback();
+        receiverWifi = new WifiReceiver();
+        registerReceiver(receiverWifi, new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
     }
 
-    public void doInback()
+    public void BluetoothConfiguration()
+    {
+        bluetoothScanResultListView = (ListView) findViewById(R.id.bluetoothScanResults);
+
+        IntentFilter filter = new IntentFilter();
+
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothReceiver receiverBluetooth = new BluetoothReceiver();
+        registerReceiver(receiverBluetooth, filter);
+
+        bluetoothArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, bluetoothDeviceNameList);
+        bluetoothScanResultListView.setAdapter(bluetoothArrayAdapter);
+    }
+
+    public void doScanning()
     {
         handler.postDelayed(new Runnable() {
 
             @Override
             public void run()
             {
-                // TODO Auto-generated method stub
-                mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                if (wifiSwitch.isEnabled())
+                    mainWifi.startScan();
+                if (bluetoothSwitch.isEnabled())
+                    bluetoothAdapter.startDiscovery();
 
-                receiverWifi = new WifiReceiver();
-                registerReceiver(receiverWifi, new IntentFilter(
-                        WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-                mainWifi.startScan();
-                doInback();
+                doScanning();
             }
         }, 12000);
 
@@ -108,7 +197,7 @@ public class MainActivity extends Activity {
 
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         mainWifi.startScan();
-        mainText.setText("Starting Scan");
+        deviceMainText.setText("Starting Scan");
         return super.onMenuItemSelected(featureId, item);
     }
 
@@ -122,28 +211,51 @@ public class MainActivity extends Activity {
         super.onResume();
     }
 
-
-
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
 
     // Broadcast receiver class called its receive method
     // when number of wifi connections changed
-
     class WifiReceiver extends BroadcastReceiver {
 
         // This method call when number of wifi connections changed
         public void onReceive(Context c, Intent intent) {
 
             sb = new StringBuilder();
-            wifiList = mainWifi.getScanResults();
-            sb.append("Wifi connections :" + wifiList.size() + "\n");
+            wifiScanResultList = mainWifi.getScanResults();
+            sb.append("Wifi connections :" + wifiScanResultList.size() + "\n");
             sb.append("Android API :" + Integer.valueOf(android.os.Build.VERSION.SDK));
-            mainText.setText(sb);
+            deviceMainText.setText(sb);
 
-            wifiSSIDScanResult.clear();
-            for (int i = 0 ; i < wifiList.size(); i++)
-                wifiSSIDScanResult.add(wifiList.get(i).SSID);
-            wifiScanResultAdapter.notifyDataSetChanged();
+            wifiSSIDList.clear();
+            for (int i = 0 ; i < wifiScanResultList.size(); i++)
+                wifiSSIDList.add(wifiScanResultList.get(i).SSID);
+            wifiArrayAdapter.notifyDataSetChanged();
         }
+    }
 
+    class BluetoothReceiver extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                //discovery starts, we can show progress dialog or perform other tasks
+                bluetoothScanResultList.clear();
+                bluetoothDeviceNameList.clear();
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                //discovery finishes, dismis progress dialog
+                Toast.makeText(getApplicationContext(), "ENDEEED", Toast.LENGTH_LONG);
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //bluetooth device found
+                BluetoothDevice device = (BluetoothDevice)intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if (!bluetoothScanResultList.contains(device) && !bluetoothDeviceNameList.contains(device.getName()))
+                {
+                    bluetoothScanResultList.add(device);
+                    bluetoothDeviceNameList.add(device.getName());
+                }
+            }
+            bluetoothArrayAdapter.notifyDataSetChanged();
+        }
     }
 }
---
+++ EnvScanner/app/src/main/java/com/example/brzydal/envscanner/WifiScanSection.java
@@ -0,0 +1,13 @@
+package com.example.brzydal.envscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WifiScanSection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi_scan_section);
+    }
+}
